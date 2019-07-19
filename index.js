591283e1ca59ba29d420da2907d3422590b09c88
aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-07-19 12:57:57 +0200

    Part_3 Exercise 3.3

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ app.use(bodyParser.json());
 
 let people = [
   {
+    id: 1,
     name: 'Arto Hellas',
-    phoneNumber: '040-123456',
-    id: 1
+    phoneNumber: '040-123456'
   },
   {
+    id: 2,
     name: 'Ada Lovelace',
-    phoneNumber: '39-44-5323523',
-    id: 2
+    phoneNumber: '39-44-5323523'
   },
   {
+    id: 3,
     name: 'Dan Abramov',
-    phoneNumber: '12-43-234345',
-    id: 3
+    phoneNumber: '12-43-234345'
   },
   {
+    id: 4,
     name: 'Mary Poppendieck',
-    phoneNumber: '39-23-6423122',
-    id: 4
+    phoneNumber: '39-23-6423122'
   }
 ];
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/people', (req, res) => {
   res.json(people);
 });
 
@@ -39,6 +39,21 @@ app.get('/info', (req, res) => {
         ${new Date()}
         </div>
     `);
+});
+
+app.get('/api/people/:id', (req, res) => {
+  let id = Number(req.params.id);
+  //   console.log(id);
+  let person = people.find(p => {
+    // console.log(p.id, typeof p.id, id, typeof id, p.id === id);
+    return p.id === id;
+  });
+  console.log(person);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 const PORT = 3001;
