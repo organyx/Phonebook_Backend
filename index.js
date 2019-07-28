151742e75ecb46750826de54369fb22868ebc82a
aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-07-28 13:43:54 +0200

    Part_3 Exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
 morgan.token(
