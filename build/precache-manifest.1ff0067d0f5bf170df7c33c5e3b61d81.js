eec796b553a724de4c2d2b93f055962637047547
aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-07-28 14:53:16 +0200

    Part_3 Exercise 3.11

---
+++ build/precache-manifest.1ff0067d0f5bf170df7c33c5e3b61d81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44d119ebe17e58e141fefe27eac5d88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "748c49db6d6617fdfa63",
+    "url": "/static/css/main.84074ae6.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "748c49db6d6617fdfa63",
+    "url": "/static/js/main.f69dedb7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-
+app.use(express.static('build'));
 app.use(cors());
 app.use(bodyParser.json());
 
