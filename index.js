1bd916ba5efd7ff0a3eb845f53bd83267790e513
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-11-06 15:05:31 +0100

    Part_3 Exercise 3.22

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
 
-const app = express();
-app.use(express.static('build'));
-app.use(cors());
-app.use(bodyParser.json());
+const app = express()
+app.use(express.static('build'))
+app.use(cors())
+app.use(bodyParser.json())
 
 
 morgan.token(
   'postBody',
   (getPostBody = req => {
-    return req.postBody;
+    return req.postBody
   })
-);
+)
 
 function assignPostBody(req, res, next) {
   //   console.log(req.method === 'POST');
   if (req.method === 'POST') {
-    req.postBody = JSON.stringify(req.body);
+    req.postBody = JSON.stringify(req.body)
   }
-  next();
+  next()
 }
 
-app.use(assignPostBody);
+app.use(assignPostBody)
 app.use(
   morgan(
     ':method :url :status :res[content-length] - :response-time ms :postBody'
   )
-);
+)
 
 let people = [
   {
@@ -54,7 +54,7 @@ let people = [
     name: 'Mary Poppendieck',
     phoneNumber: '39-23-6423122'
   }
-];
+]
 
 app.get('/api/people', (req, res) => {
   Person.find({}).then(result => {
@@ -63,7 +63,7 @@ app.get('/api/people', (req, res) => {
     })
     res.json(result)
   })
-});
+})
 
 app.get('/api/info', (req, res) => {
   Person.find({}).then(result => {
@@ -73,27 +73,27 @@ app.get('/api/info', (req, res) => {
         <br />
         ${new Date()}
         </div>
-    `);
+    `)
   })
 
-});
+})
 
 app.post('/api/people/', (req, res, next) => {
-  const body = req.body;
+  const body = req.body
   if (!body) {
     return res.status(400).json({
       error: 'content missing'
-    });
+    })
   }
 
   if (body.name === '' || undefined || (body.phoneNumber === '' || undefined)) {
-    return res.status(400).json({ error: 'some information is missing' });
+    return res.status(400).json({ error: 'some information is missing' })
   }
 
   const person = new Person({
     name: req.body.name,
     phoneNumber: req.body.phoneNumber
-  });
+  })
 
   person.save()
     .then(savedPerson => savedPerson.toJSON())
@@ -103,11 +103,11 @@ app.post('/api/people/', (req, res, next) => {
     .catch(err => {
       console.log('err', err)
       next(err)
-    });
-});
+    })
+})
 
 app.get('/api/people/:id', (req, res, next) => {
-  let id = req.params.id;
+  let id = req.params.id
   Person.findById(id).then(person => {
     if (person) {
       res.json(person.toJSON())
@@ -119,15 +119,15 @@ app.get('/api/people/:id', (req, res, next) => {
     next(err)
   })
 
-});
+})
 
 app.delete('/api/people/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then(result => {
+    .then(() => {
       res.status(204).end()
     })
     .catch(error => next(error))
-});
+})
 
 app.put('/api/people/:id', (req, res, next) => {
   const body = req.body
@@ -142,17 +142,17 @@ app.put('/api/people/:id', (req, res, next) => {
     }).catch(err => next(err))
 })
 
-const getNewId = () => {
-  const maxId = people.length > 0 ? Math.max(...people.map(p => p.id)) : 0;
+// const getNewId = () => {
+//   const maxId = people.length > 0 ? Math.max(...people.map(p => p.id)) : 0
 
-  return maxId + 1;
-};
+//   return maxId + 1
+// }
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (err, req, res, next) => {
   console.error(err.message)
@@ -168,7 +168,7 @@ const errorHandler = (err, req, res, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -6,33 +6,33 @@ const url = process.env.MONGODB_URI
 console.log('...', url)
 
 mongoose.connect(url, { useNewUrlParser: true }).then(res => {
-    console.log('Connection Successfull')
+  console.log('Connection Successfull')
 }).catch(err => {
-    console.log('Error', err)
+  console.log('Error', err)
 })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 3
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-        minlength: 8
-    },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    minlength: 8
+  },
 })
 
 personSchema.plugin(uniqVal)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObj) => {
-        returnedObj.id = returnedObj._id.toString()
-        delete returnedObj._id
-        delete returnedObj.__v
-    }
+  transform: (document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
